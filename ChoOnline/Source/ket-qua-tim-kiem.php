120f35a71439eef02ea65edc74736e7382f9f41b
PhuocVoquang <PhuocVoquang@gmail.com>
2013-06-09 12:11:40 +0000

    timkiem.php ket-qua-tim-kiem.php

---
+++ ChoOnline/Source/ket-qua-tim-kiem.php
@@ -0,0 +1,127 @@
+<?php 
+	session_start();
+	$count="";	
+?>
+<?php
+	if(isset ($_SESSION['name']))
+	{
+		$count = 1;
+		$name = $_SESSION['name'];
+		//$data =  $_SESSION['data'];
+	}
+?>
+<html>
+	<head>
+		<title>Tools Shop</title>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<!--[if IE 6]>
+		<link rel="stylesheet" type="text/css" href="css/iecss.css" />
+		<![endif]-->
+		<script type="text/javascript" src="js/boxOver.js"></script>
+	</head>
+	<body>
+<div id="main_container">
+  <?php
+	include ('header.php');
+  ?>
+  <div id="main_content">
+    <?php
+		include ('menutab.html');
+	?>
+    <!-- end of menu tab -->
+    <div class="crumb_navigation"> Navigation: <span class="current">Trang chủ</span> </div>
+    <?php
+		include ('left_content.php');
+	?>
+    <!-- end of left content -->
+    <div class="center_content">
+      <div class="oferta"> <img src="images/p1.png" width="165" height="113" border="0" class="oferta_img" alt="" />
+        <div class="oferta_details">
+          <div class="oferta_title">Power Tools BST18XN Cordless</div>
+          <div class="oferta_text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco </div>
+          <a href="http://all-free-download.com/free-website-templates/" class="prod_buy">details</a> </div>
+      </div>
+      <div class="center_title_bar">Kết quả tìm kiếm</div>
+	  <?php
+		$dem =1;
+		$tukhoa = $_POST['tukhoa'];
+		$con = mysqli_connect('localhost','root','root','choonline1');
+		$res = mysqli_query($con, "select * from san_pham where Ten_san_pham like '%$tukhoa%'");
+		$kiemtra=0;
+		while($row=mysqli_fetch_array($res))
+		{
+		if($dem<=12)
+			{
+				echo "<div class='prod_box'>";
+				echo "<div class='center_prod_box'>";
+				echo "<div class='product_title'>";
+				echo "<a href=''>";
+				echo $row['Ten_san_pham'];
+				echo "</a>";
+				echo "</div>";
+				 echo "<div class='product_img'>";
+				  echo "<a href=''>";
+				  echo "<img src='$row[Hinh_dai_dien]' alt='' border='0' width ='173' height='80'/>";
+				  echo "</a>";
+				  echo "</div>";
+				  echo "<div class='prod_price'>";
+				  echo "<span class='price'>";
+				  echo $row['Gia_ban'];
+				  echo " VNĐ";
+				  echo "</span>";
+				  echo "</div>";
+				  echo "</div>";
+				   echo "<div class='prod_details_tab'>";
+				echo "<a href='' class='prod_buy'>";
+				echo "Add to Cart";
+				echo "</a>"; 
+				echo "<a href='' class='prod_details'>";
+				echo "Details";
+				echo "</a>";
+				echo "</div>";
+				echo "</div>";
+				$dem++;
+				$kiemtra++;
+			}
+			if($kiemtra==0)
+			{
+				echo "Không có kết quả!";
+			}
+		}
+	  ?>
+    </div>
+    <!-- end of center content -->
+    <div class="right_content">
+      <?php
+		include "timkiem.php";
+	  ?>
+      <div class="shopping_cart">
+        <div class="title_box">Shopping cart</div>
+        <div class="cart_details"> 3 items <br />
+          <span class="border_cart"></span> Total: <span class="price">350$</span> </div>
+        <div class="cart_icon"><a href="http://all-free-download.com/free-website-templates/"><img src="images/shoppingcart.png" alt="" width="35" height="35" border="0" /></a></div>
+      </div>
+      <div class="title_box">Đăng nhập</div>
+      <?php
+		include ('dangnhap.php');
+	  ?>
+	  <?php
+		include ('thongtin-dangnhap.php');
+	  ?>
+      <div class="banner_adds"> <a href="http://all-free-download.com/free-website-templates/"><img src="images/bann1.jpg" alt="" border="0" /></a> </div>
+    </div>
+    <!-- end of right content -->
+  </div>
+  <!-- end of main content -->
+  <div class="footer">
+    <div class="left_footer"> <img src="images/footer_logo.png" alt="" width="89" height="42"/> </div>
+    <div class="center_footer"> Template name. All Rights Reserved 2008<br />
+      <a href="http://csscreme.com"><img src="images/csscreme.jpg" alt="csscreme" title="csscreme" border="0" /></a><br />
+      <img src="images/payment.gif" alt="" /> </div>
+    <div class="right_footer"> <a href="http://all-free-download.com/free-website-templates/">home</a> <a href="http://all-free-download.com/free-website-templates/">about</a> <a href="http://all-free-download.com/free-website-templates/">sitemap</a> <a href="http://all-free-download.com/free-website-templates/">rss</a> <a href="http://all-free-download.com/free-website-templates/">contact us</a> </div>
+  </div>
+</div>
+<!-- end of main_container -->
+<div align=center>This template  downloaded form <a href='http://all-free-download.com/free-website-templates/'>free website templates</a></div></body>
+</html>
---
+++ ChoOnline/Source/timkiem.php
@@ -0,0 +1,10 @@
+<div class="title_box">Tìm kiếm</div>
+ <div class="border_box">
+<?php
+	echo "<form action='ket-qua-tim-kiem.php' method = post class='newsletter_input'>";
+		echo "<input type='text' name='tukhoa'>";
+		echo "<input type='submit' value='Tìm'>";
+	echo "</form>";
+?>
+</div> 
+	
