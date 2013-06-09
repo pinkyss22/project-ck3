711154727b03a97bbc1e8779e5ddec97d76f5bdf
PhuocVoquang@gmail.com <PhuocVoquang@gmail.com@52b36d78-cb01-a2de-d453-cef9842002d3>
2013-06-09 13:55:11 +0000

    edit.php quan-li-san-pham.php phantrang.php

--- trunk/ChoOnline/Source/quan-li-san-pham.php
+++ trunk/ChoOnline/Source/quan-li-san-pham.php
@@ -11,158 +11,77 @@
 ?>
 <html>
 	<head>
+		<title>Tools Shop</title>
 		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-		<title>Chợ Online</title>
-		<link rel="stylesheet" type="text/css" href="css/style.css">
-		<script type="text/javascript" src="js/jquery-1.3.1.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<link href="css/nivo-slider.css" rel="stylesheet" type="text/css" />
+		<script src="js/jquery-1.5.1.js" type="text/javascript"></script>
+		<script src="js/jquery.cycle.all.latest.js" type="text/javascript"></script>
+		<script src="js/jquery.nivo.slider.pack.js" type="text/javascript"></script>
+		<script src="js/nivo.js" type="text/javascript"></script>
+		<!--[if IE 6]>
+		<link rel="stylesheet" type="text/css" href="css/iecss.css" />
+		<![endif]-->
+		<script type="text/javascript" src="js/boxOver.js"></script>
 	</head>
-	<body>
-		<div id="wrapper">
-			<div id="header">
-				<a href="index.php">
-					<img src="img/logo.gif" width="900" height="60">
-				</a>
-				<div id="login">
-					<?php
-						if($count != 1 )
-						{?>
-								<form name="flogin" action="Login.php" method="post">
-								Tài khoản: <input name="txtUser" type="txt" size="15" maxlength="20" width="15">
-								Mật khẩu: <input name="txtPassWord" type="password" size="15" maxlength="20" width="15">
-								<input type="submit" value="Đăng nhập">
-								<a href="register.php"><input type="button" value="Đăng ký"></a>
-							</form>
-						<?php
-						}
-						else
-						{
-							echo "<div>";
-							echo "Chào: "; echo "<a href='thong-tin-tai-khoan.php'>$name</a>";
-							echo "</div>";
-							echo "<div>";
-							echo "<a href='logout.php'>Thoát</a>";
-							echo "</div>";
-						}
-					?>
-				</div>
-				<!-- auto slide-->
-				<div id="gallery">
-					<a href="#" class="show">
-						<img src="images/flowing-rock.jpg" alt="Flowing Rock" width="580" height="360" title="" alt="" rel="<h3>Flowing Rock</h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "/>
-					</a>	
-					<a href="#">
-						<img src="images/grass-blades.jpg" alt="Grass Blades" width="580" height="360" title="" alt="" rel="<h3>Grass Blades</h3>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "/>
-					</a>				
-					<a href="#">
-						<img src="images/ladybug.jpg" alt="Ladybug" width="580" height="360" title="" alt="" rel="<h3>Ladybug</h3>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."/>
-					</a>
-					<a href="#">
-						<img src="images/lightning.jpg" alt="Lightning" width="580" height="360" title="" alt="" rel="<h3>Lightning</h3>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."/>
-					</a>			
-					<a href="#">
-						<img src="images/lotus.jpg" alt="Lotus" width="580" height="360" title="" alt="" rel="<h3>Lotus</h3>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo."/>
-					</a>				
-					<a href="#">
-						<img src="images/mojave.jpg" alt="Mojave" width="580" height="360" title="" alt="" rel="<h3>Mojave</h3>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."/>
-					</a>		
-					<a href="#">
-						<img src="images/pier.jpg" alt="Pier" width="580" height="360" title="" alt="" rel="<h3>Pier</h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."/>
-					</a>	
-					<a href="#">
-						<img src="images/sea-mist.jpg" alt="Sea Mist" width="580" height="360" title="" alt="" rel="<h3>Sea Mist</h3>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."/>
-					</a>
-					<a href="#">
-						<img src="images/stones.jpg" alt="Stone" width="580" height="360" title="" alt="" rel="<h3>Stone</h3>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."/>
-					</a>
-					<div class="caption"><div class="content"></div></div>
-				</div>
-				<div class="clear"></div>
-				<!-- -->
-				<img src="img/header_2.png" width="900">
-			</div>
-			<div id="siderbar">
-			<dl>
-					<dt>Danh mục sản phẩm</dt>
-					<?php
-						$con = mysqli_connect('localhost','root','root','Choonline1');
-						$result = mysqli_query($con, 'SELECT * FROM danh_muc');
-						$i=1;
-						while($row = mysqli_fetch_array($result))
-						{
-							?><dd><a href="danh-muc.php?id=<?php echo $i?>"><?php echo $row['Ten_danh_muc']?></a></dd>
-						<?php
-							$i++;
-						}
-						mysqli_close($con);
-					?>
-				</dl>
-				<dl>
-					<dt>Quản lí tài khoản</dt>
-					<dd><a href="thong-tin-tai-khoan.php">Thông tin tài khoản</a></dd>
-					<?php
-						if($_SESSION['type'] == 1)
-						{
-							echo	"<dd><a href='don-hang.php'>Đơn hàng</a></dd>";
-							echo 	"<dd><a href='Up-san-pham.php'>Up sản phẩm</a></dd>";
-							echo 	"<dd><a href='quan-li-san-pham.php'>Quản lí sản phẩm</a></dd>";
-						}
-						else if($_SESSION['type'] == 2)
-						{
-							echo 	"<dd><a href='gio-hang.php'>Giỏ hàng</a></dd>";
-						}
-					?>
-				</dl>
-			</div>
-			<div id="content">
-			<h2>Quản lí sản phẩm</h2>
-			<table border="1">
-				<tr>
-					<td><b>STT</b></td>
-					<td><b>Tên sản phẩm<b></td>
-					<td><b>Hình ảnh</b></b>
-					<td><b></b>Giá bán</td>
-					<td><b>Số lượng</b></td>
-				</tr>
-				<?php
-					$con1 = mysqli_connect('localhost','root','root','choonline1');
-					$result1 = mysqli_query($con1, "SELECT * FROM tai_khoan WHERE Ten_dang_nhap ='$name'");
-					$row1 = mysqli_fetch_array($result1);
-					$ma_tai_khoan = $row1['Ma_tai_khoan'];
-					//mysqli_close($con);
-					$dem = 1;
-					$con = mysqli_connect('localhost','root','root','choonline1');
-					$result = mysqli_query($con, "SELECT * FROM san_pham WHERE Ma_nguoi_ban ='$ma_tai_khoan'");
-					while($row = mysqli_fetch_array($result))
-					{
-						echo "<tr>";
-							echo "<td>";
-							echo "$dem";
-							echo "</td>";
-							echo "<td>";
-							echo $row['Ten_san_pham'];
-							echo "</td>";
-							echo "<td>";
-							echo "<img src='$row[Hinh_dai_dien]'>";
-							echo "</td>";
-							echo "<td>";
-							echo $row['Gia_ban'];
-							echo "</td>";
-							echo "<td>";
-							$so = $row['So_luong_ton'];
-							$i = $row['Ma_san_pham'];
-							echo "<form action ='quan-li-san-pham-edit.php?id=$i' method='post'>";
-							echo "<input type = 'number' name = 'so_luong' value ='$so' size ='8'>";
-							echo "<input type = 'submit' value = 'Sữa'>";
-							echo "</form>";
-							#echo $row['So_luong_ton'];
-							echo "</td>";
-						echo "</tr>";
-						$dem++;
-					}					
-				?>
-			</table>			
-			</div>
-			<div id="footer"></div>
-		</div>
-	</body>
+	<<body>
+<div id="main_container">
+  <?php
+	include ('header.php');
+  ?>
+  <div id="main_content">
+    <?php
+		include ('menutab.html');
+	?>
+    <!-- end of menu tab -->
+    <div class="crumb_navigation"> <span class="current">Trang chủ</span> </div>
+    <?php
+		include ('left_content.php');
+	?>
+    <!-- end of left content -->
+    <div class="center_content">
+      <div class="oferta"> <img src="images/p1.png" width="165" height="113" border="0" class="oferta_img" alt="" />
+        <div class="oferta_details">
+          <div class="oferta_title">Galaxy S3 I9300</div>
+          <div class="oferta_text"> Galaxy S3 I9300 là sản p </div>
+          <a href="http://all-free-download.com/free-website-templates/" class="prod_buy">details</a> </div>
+      </div>
+      <div class="center_title_bar">Quản lý sản phẩm</div>
+		<?php				
+			include ('phantrang.php');
+		?>
+    </div>
+    <!-- end of center content -->
+    <div class="right_content">
+      <?php
+		include "timkiem.php";
+	  ?>
+      <div class="shopping_cart">
+        <div class="title_box">Shopping cart</div>
+        <div class="cart_details"> 3 items <br />
+          <span class="border_cart"></span> Total: <span class="price">350$</span> </div>
+        <div class="cart_icon"><a href="http://all-free-download.com/free-website-templates/"><img src="images/shoppingcart.png" alt="" width="35" height="35" border="0" /></a></div>
+      </div>
+      <div class="title_box">Đăng nhập</div>
+      <?php
+		include ('dangnhap.php');
+	  ?>
+	  <?php
+		include ('thongtin-dangnhap.php');
+	  ?>
+      <div class="banner_adds"> <a href="http://all-free-download.com/free-website-templates/"><img src="images/bann1.jpg" alt="" border="0" /></a> </div>
+    </div>
+    <!-- end of right content -->
+  </div>
+  <!-- end of main content -->
+  <div class="footer">
+    <div class="left_footer"> <img src="images/footer_logo.png" alt="" width="89" height="42"/> </div>
+    <div class="center_footer"> Template name. All Rights Reserved 2008<br />
+      <a href="http://csscreme.com"><img src="images/csscreme.jpg" alt="csscreme" title="csscreme" border="0" /></a><br />
+      <img src="images/payment.gif" alt="" /> </div>
+    <div class="right_footer"> <a href="http://all-free-download.com/free-website-templates/">home</a> <a href="http://all-free-download.com/free-website-templates/">about</a> <a href="http://all-free-download.com/free-website-templates/">sitemap</a> <a href="http://all-free-download.com/free-website-templates/">rss</a> <a href="http://all-free-download.com/free-website-templates/">contact us</a> </div>
+  </div>
+</div>
+<!-- end of main_container -->
+<div align=center>This template  downloaded form <a href='http://all-free-download.com/free-website-templates/'>free website templates<
 </html>
